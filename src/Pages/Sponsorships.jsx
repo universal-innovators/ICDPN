7c44e3c45088d40d720088ef9933de386ffe5817
himanshu <himanshu.bani123@gmail.com>
2024-03-22 01:21:29 +0530

    Add sponsorship section to Sponsorships page

--- src/Pages/Sponsorships.jsx
+++ src/Pages/Sponsorships.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import ImpDates from "../components/ImpDates"
 
 const Sponsorships = () => {
   return (
-    <div>Sponsorships</div>
+    <div>
+      <hr />
+      <h2 className="sponsor-heading">Sponsorship</h2>
+      <hr />
+      <div className="flex justify-around">
+        <div className="sponsor-text-container">
+          <h4>INVITATION LETTER FOR SPONSORSHIP</h4>
+          <br />
+          <p className="font-bold">Dear Technology Leader,</p>
+          <br />
+          <p className="long-para">In the era of digital revolution where world is becoming smaller and a lot of new technologies are being launched simultaneously. The ongoing saga of the computing and communication digital revolution, the new mantra - Extend and Adapt, has produced ripples of excitement in the industry and academia! Therefore, to make a developed India and to match the speed of these above mentioned challenges joint efforts from Industry and Academia are required. Academia is doing its bit by organizing the international conference ICICC 2025 and bringing together worlds the best technological brains and ideas under one roof. This kind of initiative require lot of efforts and funding.Fifth International Conference on Innovative Computing and Communication (ICICC 2025) is organized with the objective of bringing together innovative scientists, professors, research scholars, students and industrial experts in the field of Computing and Communication to a common forum. The primary goal of the conference is to promote the exchange of innovative scientific information between researchers, developers, engineers, students, and practitioners. This conference will try to take the research to the new level with the motive of applying research into the development of startups and industry-based applications. It will also bridge the gap between industry/ Start-up requirements and research-based activities that further will give a new direction to the research happening in and around the world. Another goal is to promote the transformation of fundamental research into institutional and industrialized research and to convert applied exploration into real time application. Overall the conference will provide the researchers and attendees with prospects for national and international collaboration and networking among universities and institutions from India and abroad for promoting research. ICICC 2025 will be hosted by Shaheed Sukhdev College of Business Studies (University of Delhi) in association with National Institute of Technology Patna, a place to pursue knowledge beyond traditional boundaries. An institute initiated to impart quality education to the youth while equipping them with knowledge and skills which would enable them to leave their individual mark in the business world.</p>
+          <br />
+          <p>The committee of professionals dedicated towards the conference is striving to achieve a high quality technical program with Tracks on Innovative Computing, Innovative Communication Network and Security, and Internet of Things </p>
+          <br />
+          <p>The highlights of the conference are as follows:</p>
+          <br />
+          <ul>
+            <li>Top national and international keynote speakers</li>
+            <li>Regular research paper presentation sessions</li>
+            <li>Amalgam of the best national and international minds</li>
+            <li>Integration of experienced and young researchers</li>
+            <li>Technical poster paper representation among the best students of India</li>
+            <li>Startup funding event for MAKE IN INDIA mission</li>
+            <li>Invitation to possible investors for some of the best ideas</li>
+            <li>Exhibits</li>
+            <li>International parallel Workshops from national and international researchers and industry experts</li>
+            <li>Expected audience of minimum 500, comprised of reseasrchers, scientists and people from academia and industry</li>
+            <li>Last year version of the conference being held at Czech Republic, Europe, received papers of around 2600 authors from 25 different countries</li>
+          </ul>
+          <br />
+          <p>As a spearheading force in the industry driving forward this revolution, ICICC 2025 provides just the right platform and visibility to showcase your company, its vision and its technological offerings to a wide spectrum of professionals. Your participation will give an opportunity to nurture the talent pool and spur a collaborative atmosphere necessary for progress.
+Kindly see the various sponsorship packages and offers and give us an opportunity to collaborate on this initiative and work together for its success right from the outset</p>
+          <br />
+          <br />
+          <p className="font-bold">Yours Sincerely,</p>
+          <p className="font-bold">General Chair</p>
+          <br />
+          <a href="">Sponsorship</a>
+        </div>
+        <div>
+          <ImpDates />
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Pages/poster_paper.jsx
@@ -0,0 +1,34 @@
+
+const Poster_paper = () => {
+  return (
+    <div>
+        
+        <hr />
+        <div className="flex justify-around">
+            <div>
+                <p>The scope of this conference is to provide a platform for researchers, engineers, academicians, and industry professionals from all over the world to present their research results and development activities in various topics of Engineering and Technology. It allows participants to discuss the recent developments in the solidification of computer science and management and review challenges faced by the community in the 21st century.</p>
+                <ol>
+                    <li>The Original unpublished Research Papers, Articles and working papers having a maximum length of 8 pages on the topics related to the theme are invited for Poster Paper presentations in the conference proceedings.</li>
+                    <li>Kindly ensure your paper is formatted s (not exceeding 8 pages written in A4 size).All papers must be submitted online via <a href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FICICC2025">Microsoft CMT Submission Portal</a> </li>
+                    <li>All submissions will be thoroughly peer-reviewed by experts based on originality, significance and clarity.</li>
+                    <li>Only papers presenting original content with novel research results or successful innovative applications will be considered for Poster Paper Presentation.</li>
+                </ol>
+                <h4>Plagiarism Policy</h4>
+                <ol>
+                    <li>The paper prior to submission should be checked for plagiarism from licensed plagiarism softwares like Turnitin/iAuthenticate etc. The similarity content should not exceed 20% (in any case either self contents or others). Further, you have to strictly implement the following ethical guidelines for publication.</li>
+                    <li>Any form of self-plagiarism or plagiarism from others work(s) should not be there in an article.
+If any model / concept / figure / table / data / conclusive comment by any previously published work is used in your article, you should properly cite a reference to the original work.</li>
+                    <li>Also language of explaining it should not be same as language of the work from which you have adopted it.
+If you are using any copyrighted material, you should acquire prior permission from the copyright holder.</li>
+                </ol>
+            </div>
+            <div>
+                <h3>Important Dates</h3>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Poster_paper
