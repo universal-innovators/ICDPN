03a2b9fdf92721a121d20f92a1e9f70e18e441fe
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2024-05-22 01:10:19 +0530

    committee changes and improvements

--- src/Pages/SteeringCommittee.jsx
+++ src/Pages/SteeringCommittee.jsx
@@ -10,237 +10,116 @@ const SteeringCommittee = () => {
       <hr />
       <div className="sm:flex justify-around gap-7">
         <div className="flex flex-wrap justify-around mt-8 mb-8 gap-[16px]">
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
             <h3>Patron(s):</h3>
             <hr />
             <p>Doc. Ing. Vojtech Stehel</p>
             <p>
               (Rector, Institute of Technology and Business in České Budějovice)
-            </p>
-            <br />
-            <p>Prof. (Dr). Rominder Kaur Randhawa</p>
-            <p>(Director, Guru Tegh Bahadur Institute of Technology)</p>
-            <br />
-            <p>Prof. Luis Loures</p>
-            <p>
-              (President, Portalegre Polytechnic University, Portalegre,
-              Portugal)
-            </p>
-            <br />
-            <p>Prof Madhu Pruthi</p>
-            <p>Principal, Keshav Mahavidyalaya</p>
-            <br />
-            <p>Prof. (Dr.) Mayank Garg</p>
-            <p>(Director, ITS Engineering College, Greater Noida)</p>
+            </p>          
+            
+            
           </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
             <h3>General Chair(s):</h3>
             <hr />
-            <p>Prof. Aboul Ella Hassanien</p>
-            <p>
-              (College of Business Administration, Kuwait University, Kuwait)
-            </p>
-            <br />
-            <p>Oscar Castillo</p>
-            <p>(Tijuana Institute Technology, Tijuana, Mexico )</p>
-            <br />
-            <p>Prof. Sergio Duarte Correia</p>
-            <p>(Polytechnic Institute of Portalegre, Portugal)</p>
-            <br />
-            <p>Jan Valicek</p>
-            <p>(Institute of Technology and Business in České Budějovice)</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Honorary Chair(s):</h3>
-            <hr />
-            <p>Prof. Janusz Kacprzyk</p>
-            <p>
-              (Polish Academy of Sciences, Systems Research Institute, Poland )
-            </p>
-            <br />
-            <p>Prof. Vaclav Snasel, Rector</p>
-            <p>(VSB-Technical University of Ostrava, Czech Republic)</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Conference Chair(s):</h3>
-            <hr />
+            <p>Prof. Dr. Bal Virdee</p>
+              <p>(London Metropolitan University)</p>
+              <br />
             <p>Prof. A.K. Singh</p>
             <p>(National Institute of Technology Kurukshetra, India)</p>
             <br />
+            
+            
+          </div>
+          
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
+            <h3>Conference Chair(s):</h3>
+            <hr />
+            <p>Prof. Sergio Duarte Correia</p>
+            <p>(Polytechnic Institute of Portalegre, Portugal.)</p><br/>
+            <p>Prof. Dr. Isabel de la Torre Diez
+</p>
+            <p>(University of Valladolid, Spain)</p> 
+            <br />
             <p>Dr. João Manuel R. S. Tavares</p>
-            <p>(Universidade do Porto (FEUP), PORTUGAL)</p>
-            <br />
-            <p>Prof. Joel J P C Rodrigues</p>
-            <p>(National Institute of Telecommunications (Inatel), Brazil)</p>
-            <br />
+            <p>(Universidade do Porto (FEUP), PORTUGAL)</p><br/>
+           
+            
+          </div>
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
+            <h3>Convener(s):</h3>
+            <hr />
             <p>Roman Danel</p>
             <p>(Institute of Technology and Business in České Budějovice)</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Technical Program Chair(s):</h3>
-            <hr />
-            <p>Prof. Giancarlo Fortino</p>
-            <p>(Università della Calabria, Italy)</p>
-            <br />
-            <p>Prof. Sanjay Mishra</p>
-            <p>(Covenant University, Nigeria)</p>
-            <br />
+            <br/>
             <p>Dr. Zdzislaw Polkowski</p>
             <p>
               (The Karkonosze University of Applied Sciences in Jelenia Góra,
               Poland)
             </p>
-            <br />
-            <p>Valentina Emilia Balas</p>
-            <p>(Aurel Vlaicu University of Arad, Romania)</p>
-            <br />
-            <p>Prof Punam Bedi</p>
-            <p>(Department of Computer Science, University of Delhi)</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Organising Chair(s):</h3>
-            <hr />
-            <p>Marta Harnicarova</p>
-            <p>(Institute of Technology and Business in České Budějovice)</p>
-            <br />
-            <p>Dr. Ashish Khanna</p>
-            <p>
-              (Maharaja Agrasen Institute of Technology (GGSIPU), New Delhi.)
-            </p>
-            <br />
-            <p>Prof. Sergio Duarte Correia</p>
-            <p>(Polytechnic Institute of Portalegre, Portugal.)</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Local Organising Chair(s):</h3>
-            <hr />
-            <p>Prof. (Dr.) Monika Jain</p>
-            <p>(HOD - ECE, ITS Engineering College, Greater Noida)</p>
-            <br />
-            <p>Dr. Setu Garg</p>
-            <p>
-              (Associate Professor, ECE, ITS Engineering College, Greater Noida)
-            </p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Convener(s):</h3>
-            <hr />
-            <p>Dr. Deepak Gupta</p>
-            <p>
-              (Maharaja Agrasen Institute of Technology (GGSIPU), New Delhi)
-            </p>
-            <br />
-            <p>Dr. Utku Kose</p>
-            <p>(Suleyman Demirel University, Isparta, Turkey)</p>
-            <br />
-            <p>Dr. Jameel Ahamed</p>
-            <p>
-              (Maulana Azad National Urdu University (MANUU), Hyderabad, India)
-            </p>
-            <br />
-            <p>Parminder Singh Bedi (HOD, IT department)</p>
-            <p>Guru Tegh Bahadur Institute of Technology</p>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Publicity Chair(s):</h3>
-            <hr />
-            <p>Dr. Prayag Tiwari</p>
-            <p>(Aalto University, Finland)</p>
-            <br />
-            <p>Dr. Ahmed J. Obaid</p>
-            <p>(University of Kufa, Iraq)</p>
-            <br />
-            <p>Dr. Akshi Kumar</p>
-            <p>(Manchester Metropolitan University, UK)</p>
-            <br />
-            <p>Prof. Pradeep Malik</p>
-            <p>(KIIT University, India)</p>
-            <br />
-            <p>Dr. Sushil Kumar Singh</p>
-            <p>(Marwadi University, Rajkot, Gujrat, India)</p>
+            
+           
           </div>
           <div className="flex flex-col gap-4">
-            <div className="testimonials h-full w-[385px] bg-neutral-100 text-secondary-400">
+            <div className="testimonials h-full w-[100%] bg-neutral-100 text-secondary-400">
               <h3>Publication Chair(s):</h3>
               <hr />
               <p>Prof. Abhishek Swaroop</p>
               <p>(Bhagwan Parshuram Institute of Technology, India)</p>
               <br />
-              <p>Prof. Sérgio Duarte Correia</p>
-              <p>(Polytechnic Institute of Portalegre, Portugal. )</p>
-              <br />
-              <p>Prof. P. Sanjeevi kumar</p>
-              <p>(Aarhus University, Herning, Denmark)</p>
+              <p>Jan Valicek</p>
+            <p>(Institute of Technology and Business in České Budějovice)</p>
+              
+              
             </div>
-            <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-              <h3>Co-Convener(s):</h3>
-              <hr />
-              <p>Moolchand Sharma</p>
-              <p>
-                (Maharaja Agrasen Institute of Technology (GGSIPU), New Delhi)
-              </p>
-              <br />
-              <p>Dr Aman Dureja</p>
-              <p>
-                (Bhagwan Parshuram Institute of Technology, New Delhi, India)
-              </p>
-              <br />
-              <p>Dr Ajay Dureja</p>
-              <p>(Bharati Vidyapeeth's College of Engineering, Delhi)</p>
-              <br />
-              <p>Dr. Gulshan Shrivastava</p>
-              <p>(Galgotia University, Gr. Noida, India)</p>
-              <br />
-              <p>Dr. Aditya Khamparia</p>
-              <p>(Babasaheb Bhimrao Ambedkar University, Lucknow, India)</p>
-            </div>
-          </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Editorial Board:</h3>
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
+            <h3>Technical Program Chair(s):</h3>
             <hr />
-            <p>Prof. Aboul Ella Hassanien</p>
+            <p>Omer Rana</p>
+            <p>(Cardiff University, UK)</p>
+            <br/>
+            <p>Prof Punam Bedi</p>
+            <p>(Department of Computer Science, University of Delhi)</p>
+            <br/>
+            <p>Dr. Richa Sharma</p>
+            <p>(London Metropolitan University)</p>
+            <br/>
+            <p>Dr. Momina Shaheen</p>
+            <p>(Department of Computing, School of Arts Humanities and Social Sciences, University of Roehampton, London SW15 5PJ, United Kingdom.
+
+)</p>
+            
+     
+            
+          </div>
+                 
+          <div className="testimonials w-[100%] bg-neutral-100 text-secondary-400">
+            <h3>Publicity Chair(s):</h3>
+            <hr />
+            <p>Dr. Akshi Kumar</p>
+            <p>(Manchester Metropolitan University, UK)</p>
+            <br />
+            <p>Dr. Sandra Fernando</p>
+            <p>(London Metropolitan University)</p>
+            <br />         
+
+            <p>Dr. Jameel Ahamed</p>
             <p>
-              (College of Business Administration, Kuwait University, Kuwait)
+              (Maulana Azad National Urdu University (MANUU), Hyderabad, India)
             </p>
             <br />
-            <p>Dr Gurmeet Singh</p>
-            <p>(Guru Tegh Bahadur institute of Technology, Delhi, India)</p>
+            <p>Mr. Moolchand Sharma</p>
+            <p>(Maharaja Agrasen Institute of Technology, Delhi)</p>
+            <br />      
+          
+           
+            <p>Dr. Mamoona Humayun</p>
+            <p>(Department of Information Systems, College of Computer and Information Sciences, Jouf University, Sakaka, Saudi Arabia)</p>
           </div>
-          <div className="testimonials w-[385px] bg-neutral-100 text-secondary-400">
-            <h3>Organising Team:</h3>
-            <hr />
-            <p>Prof Vinita Jindal</p>
-            <p>Keshav Mahavidyalaya, University of Delhi</p>
-            <br />
-            <p>Prof Hema Banati</p>
-            <p>Dayal Singh College, University of Delhi</p>
-            <br />
-            <p>Dr Anjali Thukral</p>
-            <p>Keshav Mahavidyalaya, University of Delhi</p>
-            <br />
-            <p>Dr Bhavna Gupta</p>
-            <p>Keshav Mahavidyalaya, University of Delhi</p>
-            <br />
-            <p>Ms Richa Gupta</p>
-            <p>Keshav Mahavidyalaya, University of Delhi</p>
-            <br />
-            <p>Rahul Arora</p>
-            <p>B.Sc. (H) Computer Science, 2nd Year, KMV</p>
-            <br /> <p>Nancy Gupta</p>
-            <p>B.Sc. (H) Computer Science, 2nd Year, KMV</p>
-            <br />
-            <p>Anjali</p>
-            <p>B.Sc. (H) Computer Science, 1st Year, KMV</p>
-            <br />
-            <p>Ayser Islam</p>
-            <p>B.Sc. (H) Computer Science, 1st Year, KMV</p>
-            <br />
-            <p>Jhalak Arora</p>
-            <p>B.Sc. (H) Computer Science, 1st Year, KMV</p>
-            <br />
-            <p>Mohit Kumar</p>
-            <p>B.Sc. (H) Computer Science, 1st Year, KMV</p>
-          </div>
+          
+            
+          </div>         
         </div>
         <div className="mt-8 min-w-fit">
           <ImpDates />
---
+++ src/static/advcm.js
@@ -0,0 +1,182 @@
+const advcm=[
+    {
+      name: 'Prof Roshan G. Ragel',
+      info: 'University of Peradeniya, Sri Lanka'
+    },
+    {
+      name: 'Prof(Dr.) Marius Balas',
+      info: 'Aurel Vlaicu University of Arad, Romania'
+    },
+    {
+      name: 'Prof. (Dr.) M Shamim Kaiser',
+      info: 'Jahangirnagar University, Bangladesh'
+    },
+    {
+      name: 'Prof(Dr.) Pawel Gren',
+      info: 'Jan Wyzykowski University, Poland'
+    },
+    {
+      name: 'Dr. Catarina Moreira',
+      info: 'Queensland University of Technology, Australia'
+    },
+    {
+      name: 'Prof. (Dr.) João Manuel RS Tavares',
+      info: 'Professor Associado com Agregação FEUP - DEMec'
+    },
+    {
+      name: 'Prof. (Dr.) Tatiana Kalganova',
+      info: 'Brunel University London, UK'
+    },
+    { name: 'Dr. Nhu Gia Nguyen', info: 'Duy Tan University, Vietnam' },
+    {
+      name: 'Prof(Dr.) Jens Bo Holm',
+      info: 'Nielson Aalborg University, Denmark'
+    },
+    {
+      name: 'Dr. Dusanka Boskovic',
+      info: 'University of Sarajevo, Sarajevo, Bosnia'
+    },
+    {
+      name: 'Dr. Stefania Tomasiello',
+      info: 'University of Tartu, Estonia'
+    },
+    {
+      name: 'Dr. Tadeusz Kierzyk',
+      info: 'Prof UJW (Rector of Jan Wyzykowski University, Poland)'
+    },
+    { name: 'Dr. Shikun Zhou', info: 'University of Portsmouth, UK' },
+    {
+      name: 'Dr. Aida Mustapha',
+      info: 'Universiti Tun Hussein Onn Malaysia'
+    },
+    {
+      name: 'Prof. (Dr.) R. Simon Sherratt',
+      info: 'University of Reading, UK'
+    },
+    {
+      name: 'Prof. (Dr.) Alfredo Cuzzocrea',
+      info: 'University of Calabria, Italy'
+    },
+    {
+      name: 'Dr. Naresh Kumar',
+      info: 'PhD, Assistant Professor, Computer Science, DMPS, College of Arts and Sciences, University of Nizwa, Oman'
+    },
+    {
+      name: 'Prof. (Dr.) Dharm Singh Jat',
+      info: 'Namibia University of Science and Technology'
+    },
+    {
+      name: 'Prof(Dr.) Gerhard-Wilhelm Weber',
+      info: 'Poznan University of Technology, Poland'
+    },
+    {
+      name: 'Prof. (Dr.) Mike Hinchey',
+      info: 'University of Limerick, Ireland'
+    },
+    {
+      name: 'Dr. Nilgün Şengöz',
+      info: 'Burdur Mehmet Akif Ersoy University, Burdur/TURKEY'
+    },
+    {
+      name: 'Prof. (Dr.) Mufti Mahmud',
+      info: 'Computer Science, Nottingham Trent, UK'
+    },
+    {
+      name: 'Prof(Dr.) David Camacho',
+      info: 'Associate Prof, Universidad Autonoma de Madrid, Spain'
+    },
+    {
+      name: 'Prof. (Dr.) Chakchai So-In',
+      info: 'Khon Kaen University, Thailand'
+    },
+    { name: 'Prof. (Dr.) Omar Rana', info: 'Cardiff University, UK' },
+    {
+      name: 'Dr. Frank H.F. Leung',
+      info: 'Associate Professor Leader, BSc in IMT Programme, Department of Electronic & Information Engineering, Hong Kong Polytechnic University, Hung Hom, Kowloon, Hong Kong'
+    },
+    {
+      name: 'Dr. Steve S.H. Ling',
+      info: 'PhD SMIEEE Senior Lecturer School of Electrical, Mechanical and Mechatronic Systems, Centre for Health Technologies, Faculty of Engineering and IT University of Technology, Sydney'
+    },
+    {
+      name: 'Dr. Uzzal Sharma',
+      info: 'Don Bosco University, Assam, India'
+    },
+    { name: 'Dr. Milan Tuba', info: 'Sinergija University, Serbia' },
+    {
+      name: 'Prof. (Dr.) Belfin Robinson',
+      info: 'University of North Carolina, USA'
+    },
+    {
+      name: 'Dr. Ahmed A. Elngar',
+      info: 'Assistant professor Faculty of Computers and Information, Beni-Suef University, Beni Suef, Salah Salem Str., 62511 Egypt'
+    },
+    {
+      name: 'Prof(Dr.) Tiziana Catarci',
+      info: 'Sapienza University, Rome, Italy'
+    },
+    {
+      name: 'Prof S.M. Hasan Mahmud',
+      info: 'American International University'
+    },
+    {
+      name: 'Prof. (Dr.) Jyoti Choudrie',
+      info: 'University of Hertfordshire, UK'
+    },
+    {
+      name: 'Prof(Dr.) Huseyin Irmak',
+      info: 'Cankiri Karatekin University, Turkey'
+    },
+    {
+      name: 'Prof. (Dr.) Punam Bedi',
+      info: 'Senior Professor, Department of Computer Science, University of Delhi'
+    },
+    {
+      name: 'Prof(Dr.) Ladjel Bellatreche',
+      info: 'Poitiers University, France'
+    },
+    {
+        name: 'Dr. Mamoona Humayun',
+        info: 'Department of Information Systems, College of Computer and Information Sciences, Jouf University, Sakaka, Saudi Arabia'
+      },
+
+    {
+        name: 'Omer Rana',
+        info: 'Cardiff University, UK'
+      },
+    {
+      name: 'Prof. (Dr.) Sheng Lung Peng',
+      info: 'National Taipei University of Business, Taiwan'
+    },
+    { name: 'Dr. Alina Miron', info: 'Brunel University, UK' },
+    {
+      name: 'Prof. (Dr.) Jose Quenum',
+      info: 'Namibia University of Science and Technology, Namibia'
+    },
+    {
+      name: 'Prof(Dr.) Joanna Jozefowska',
+      info: 'Pro-Rector for Research, Poznan University of Technology'
+    },
+    {
+      name: 'Prof(Dr.) Cenap Ozel',
+      info: 'King Abdulaziz University, Saudi Arabia'
+    },
+    {
+        name: 'Dr. Sandra Fernando',
+        info: 'London Metropolitan University'
+      },
+    {
+      name: 'Prof (Dr.) Vincenzo Piuri',
+      info: 'University of Milan, Italy'
+    },
+    {
+      name: 'Prof. (Dr.) Saurabh Singh',
+      info: 'Dongguk University, South Korea'
+    },
+    {
+      name: 'Dr. Atta ur Rehman Khan',
+      info: 'King Saud University, Riyadh'
+    }
+  ]
+  
+export default advcm;
