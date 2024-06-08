9b3e40f83094b84ba02c11661eb0c88a2c478695
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2024-06-09 01:10:59 +0530

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,31 @@
-// import path from "path";
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   build: {
-//     rollupOptions: {
-//       input: 'index.html',
-//     },
-//   },
-//   server: {
-//     historyApiFallback: true,
-//   },
-// });
-
-// vite.config.js
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
   base: '/ICDPN/', // replace 'my-vite-app' with your repository name
 });
+
+// vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/ICDPN/', // replace 'my-vite-app' with your repository name
+// });
 
