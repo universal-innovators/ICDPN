fe2bb8812569e1cab961da0781923e6bd41416be
Pra1802 <pranavchadha1245@gmail.com>
2024-06-18 00:16:38 +0530

    edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import ImpDates from "@/components/ImpDates";
 import Conference from "../components/Conference";
 import Slider from "../components/Slider";
 import Notices from "@/components/Notices";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
   
@@ -9,6 +10,19 @@ const Home = () => {
     <>
       <Slider />
       <Conference />
+      
+      <marquee className=" sm:mx-11 text-2xl text-violet-600">CONFERENCE WILL BE ORGANISED IN HYBRID MODE. PAPER REGISTERED FOR ONLINE PRESENTATION WILL BE GIVEN 40% DISCOUNT ON REGISTRATION FEES.</marquee>
+      <figure className="w-[20rem] border rounded-md shadow-xl shadow-gray-600 m-10 p-10 border-black h-[20rem] mx-auto"><img src="https://icdam-conf.com/assets/images/oneAPI.jpg" className=""></img>
+      <figcaption className="mx-auto text-center">Intel® oneAPI<br/></figcaption>
+      </figure>
+      <div className=" p-11 rounded-tl-3xl rounded-br-3xl m-11 mx-[4rem] bg-violet-600 text-white text-lg"><b className="text-2xl">Highlights</b><br/>
+1)The conference focusses in bridging the gap between Industry and academia by organizing the workshops by the industry experts on latest technologies for the participants.<br/>
+2)The conference is anticipated to attract a large number of high quality submissions and stimulate the cutting-edge research discussions among many academic pioneering researchers, scientists, industrial engineers, students from all around the world.<br/>
+3)Another goal of this conference is to provide opportunities for academics to receive informal in-depth feedback through discussions, and to enable them to establish contact with professionals in various countries and institutions and also to encourage regional and international communication and collaboration.<br/>
+4)Provide common platform for academic pioneering researchers, scientists, engineers and students to share their views and achievements.<br/>
+5)Focus on innovative issues at international level by bringing together the experts from different countries.</div>
+      <marquee className=" sm:mx-11 text-2xl text-violet-600">ALL REGISTERED PAPERS WILL BE PUBLISHED IN SCOPUS PROCEEDING AND EXTENDED PAPERS WILL BE SENT TO SCI AND SCOPUS JOURNALS.</marquee>
+
       <div className="container dateNotice">
         <ImpDates />
         <Notices />
