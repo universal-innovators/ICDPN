3667ab641b7611c3b687ce7a6607f2ce9ee2f9fe
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2026-06-02 22:44:30 +0530

    ss

--- src/Pages/Awards.jsx
+++ src/Pages/Awards.jsx
@@ -307,7 +307,7 @@ const Awards = () => {
         </p>
         <div className="space-y-2">
           <a
-            href="https://forms.gle/T4JrS18P7ZZhgpAq9"
+            href="https://forms.gle/sVFPggej4h1F5PVK7"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
